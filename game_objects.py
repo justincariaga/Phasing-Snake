b958dab72d7b560c348b72e7780ffc931af08f1b
Justin Cariaga <tin.gab.cariaga@gmail.com>
2020-03-13 13:16:08 +0800

    aligned snake to grid

--- game_objects.py
+++ game_objects.py
@@ -23,6 +23,7 @@ class Snake:
 		self.speed = 50
 		
 	def update(self, window_width, window_height, space):
+		multiplier = 50
 		space.append(self.rects[-1].topleft)
 		for i in range(-1, -len(self.sprites), -1):
 			self.rects[i].center = self.rects[i - 1].center
@@ -30,17 +31,17 @@ class Snake:
 		self.rects[0].left += self.dx * self.speed
 		self.rects[0].top += self.dy * self.speed
 		if self.rects[0].left >= window_width:
-			self.rects[0].top = random.randrange(0, window_height)
-			self.rects[0].left = random.randrange(0, window_width // 2)
+			self.rects[0].top = random.randrange(0, window_height // 2) // multiplier * multiplier
+			self.rects[0].left = random.randrange(0, window_width // 2) // multiplier * multiplier
 		if self.rects[0].right <= 0:
-			self.rects[0].right = random.randrange(window_width // 2, window_width)
-			self.rects[0].bottom = random.randrange(0, window_height)
+			self.rects[0].right = random.randrange(window_width // 2, window_width) // multiplier * multiplier
+			self.rects[0].bottom = random.randrange(0, window_height // 2) // multiplier * multiplier
 		if self.rects[0].top >= window_height:
-			self.rects[0].top = random.randrange(0, window_height // 2)
-			self.rects[0].right = random.randrange(0, window_width)
+			self.rects[0].top = random.randrange(0, window_height // 2) // multiplier * multiplier
+			self.rects[0].right = random.randrange(0, window_width // 2) // multiplier * multiplier
 		if self.rects[0].bottom <= 0:
-			self.rects[0].bottom = random.randrange(window_height // 2, window_height)
-			self.rects[0].left = random.randrange(0, window_width)
+			self.rects[0].bottom = random.randrange(window_height // 2, window_height) // multiplier * multiplier
+			self.rects[0].left = random.randrange(0, window_width // 2) // multiplier * multiplier
 
 		try:
 			space.remove(self.rects[0].topleft)
